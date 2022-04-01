69cb7fb9c217ee5e99c2cb33b1da77913852c8ba
PedroConcon <pedroconcon1309@gmail.com>
2022-04-01 08:49:54 -0300

    projeto tela de pacientes

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,28 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+    
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET","http://api-pacientes.herokuapp.com/pacientes");
+
+  xhr.addEventListener("load", function(){
+    var erroAjax = document.querySelector("#erro-ajax");  
+    if(xhr.status == 200){
+      erroAjax.classList.add("invisivel");
+      var resposta = xhr.responseText;
+      var pacientes = JSON.parse(resposta);
+    
+      pacientes.forEach(paciente => {
+        adicionaPacienteNaTabela(paciente);  
+      });
+    }else{
+      console.log(xhr.status);
+      console.log(xhr.response);         
+      erroAjax.classList.remove("invisivel");
+    }
+    
+    
+  });
+
+  xhr.send();
+})
